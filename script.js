03c4fc6a85109e173e01717145a51ed54fb8c794
Laci Leal Junior <93396326+LaciJr@users.noreply.github.com>
2021-11-10 15:12:53 -0300

    Alterações para usar LI inves de DIV

--- script.js
+++ script.js
@@ -3,17 +3,25 @@ const btAdd = document.getElementById("btnAdd");
 const btRemove = document.getElementById("btnRemove");
 var key = 0;
 
+//Adiciona os itens do localstorage
+var tamanhoLista = localStorage.getItem('key')
+for (i = 0; i =< tamanhoLista; i++) {
+
+}
+
 //Adicionar nova tarefa
 btnAdd.addEventListener('click', event => {
+    localStorage.removeItem ('key', key);
     key++;
+    localStorage.setItem ('key', key);
     let valorTarefa = tarefa.value.trim()
     localStorage.setItem (key, valorTarefa);
-    let novaDiv = document.createElement("div");
-    novaDiv.className = 'linha'
+    let novaLi = document.createElement("li");
+    novaLi.className = 'linha'
     let novoP = document.createElement("p");
     novaDiv.appendChild(novoP);
     novoP.innerHTML = valorTarefa;
-    let divEntrada = document.getElementById("divEntrada");
-    divEntrada.insertAdjacentElement("afterend", novaDiv);
+    let listaEntrada = document.getElementById("listaEntrada");
+    divEntrada.insertAdjacentElement("afterbegin", novaLi);
 });
 
