a63f11817dbf129c40aa9828545226618b0902b8
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2021-11-11 02:23:56 -0300

    adiciona funcionamento JSON e localstorage

--- script.js
+++ script.js
@@ -1,24 +1,111 @@
 const tarefa = document.getElementById("entrada");
-const btAdd = document.getElementById("btnAdd");
-const btRemove = document.getElementById("btnRemove");
-var key = 0;
+const btnAdd = document.getElementById("btnAdd");
+const listaEntrada = document.getElementById("listaEntrada");
 
-//Adiciona os itens do localstorage
-var tamanhoLista = localStorage.getItem('key')
+//pega do storage
+let storage = localStorage.getItem('atividades');
+var listaRestaurada = [];
+
+//carrega os itens do storage
+if (storage) {
+    listaRestaurada = JSON.parse(storage)
+    for(i = 0; i < listaRestaurada.length; i++) {
+        let itensAntigos = listaRestaurada[i];
+        carregar(itensAntigos);
+    };
+};
+
+//função para adicionar itens na lista sem adicionar no storage
+function carregar(item) {
+    let novaLi = document.createElement("li");
+    novaLi.className = `linha ${item.id}`;
+    
+    //adiciona o checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = 'checkbox';
+    checkbox.className = `check check${item.id}`;
+    checkbox.onclick = `riscar(${item.id})`;
+    novaLi.appendChild(checkbox);
+    
+    //adiciona o label com a tarefa
+    let novaLabel = document.createElement("label");
+    novaLabel.setAttribute('for', `check${item.id}`);
+    novaLi.appendChild(novaLabel);
+    novaLabel.innerHTML = item.valor;
+
+    //adiciona o botão de remover
+    let novoBtn = document.createElement("button");
+    novoBtn.id = 'btnRemove';
+    novoBtn.className =  `btnRemove${item.id}`
+    novoBtn.innerHTML = '-';
+    novoBtn.onclick = `remover(${item.id})`;
+    novaLi.appendChild(novoBtn);
+    
+    //insere item na lista
+    listaEntrada.insertAdjacentElement("afterbegin", novaLi);
+}
+
+//função para adicionar itens na lista e no storage
+function novaLinha (item) {
+    let novaLi = document.createElement("li");
+    novaLi.className = `linha ${item.id}`;
+    
+    //adiciona o checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = 'checkbox';
+    checkbox.id = `check check${item.id}`;
+    //checkbox.onclick = riscar(item.id);
+    novaLi.appendChild(checkbox);
+    
+    //adiciona o label com a tarefa
+    let novaLabel = document.createElement("label");
+    novaLabel.setAttribute('for', `check${item.id}`);
+    novaLi.appendChild(novaLabel);
+    novaLabel.innerHTML = item.valor;
+
+    //adiciona o botão de remover
+    let novoBtn = document.createElement("button");
+    novoBtn.id = 'btnRemove';
+    novoBtn.className =  `btnRemove${item.id}`
+    novoBtn.innerHTML = '-';
+    //document.getElementById('btnRemove').onclick = remover(item.id);
+    novaLi.appendChild(novoBtn);
+    
+    //insere item na lista
+    listaEntrada.insertAdjacentElement("afterbegin", novaLi);
+    listaRestaurada.push(item);
+
+    //converte para JSON
+    const listaJSON = JSON.stringify(listaRestaurada);
+
+    //insere no localstorage
+    localStorage.setItem('atividades', listaJSON);
+    }
+
+//limpar a entrada depois de inserir item 
+function limparEntrada () {
+    tarefa.value = '';
+}
 
 //Adicionar nova tarefa
 btnAdd.addEventListener('click', event => {
-    localStorage.removeItem ('key', key);
-    key++;
-    localStorage.setItem ('key', key);
-    let valorTarefa = tarefa.value.trim()
-    localStorage.setItem (key, valorTarefa);
-    let novaLi = document.createElement("li");
-    novaLi.className = 'linha'
-    let novoP = document.createElement("p");
-    novaLi.appendChild(novoP);
-    novoP.innerHTML = valorTarefa;
-    let listaEntrada = document.getElementById("listaEntrada");
-    listaEntrada.insertAdjacentElement("afterbegin", novaLi);
+    if (tarefa.value != '') {
+        const item = {
+            id: parseInt(listaRestaurada.length),
+            valor: tarefa.value,
+            feito: false,
+        }
+        novaLinha(item);
+        limparEntrada(); 
+    } 
+    else {
+        window.alert('É necessário inserir uma tarefa!');
+    }
 });
+
+function remover(id) {
+    const removido = document.getElementsByClassName(`${id}`);
+    listaEntrada.removeChild(removido);
+}
+
 
